d35eee395337f59d0c8cbcde11faa167f467f737
Obua Denis Daniel <dd.obua@gmail.com>
2024-02-23 15:55:30 +0300

    Add hover effect to buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -8,7 +8,13 @@ const Calculator = () => {
   const handleClick = (e) => {};
 
   // prettier-ignore
-  const btnText = ['AC','+/-','%','÷','7','8','9','x','4','5','6','-','1','2','3','+','0','.','=']
+  const btnText = [
+    'AC','+/-','%','÷',
+    '7','8','9','x',
+    '4','5','6','-',
+    '1','2','3','+',
+    '0','.','='
+  ]
 
   return (
     <article>
